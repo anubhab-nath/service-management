dbb143619f7b9a03b0c199d4f4023019e88d3245
Anubhab Nath <reachanubhav24@gmail.com>
2023-11-12 01:34:09 +0530

    getAllSps - Functional tests

--- src/test/java/com/example/servicemanagement/TestUtils.java
+++ src/test/java/com/example/servicemanagement/TestUtils.java
@@ -1,8 +1,5 @@
 package com.example.servicemanagement;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * Contains methods to return repeating stubs and utility methods
 * */
--- src/test/java/com/example/servicemanagement/controllers/SpControllerTest.java
+++ src/test/java/com/example/servicemanagement/controllers/SpControllerTest.java
@@ -2,62 +2,73 @@ package com.example.servicemanagement.controllers;
 
 import com.example.servicemanagement.dtos.RequestSpDto;
 import com.example.servicemanagement.dtos.ResponseSpDto;
+import com.example.servicemanagement.models.Category;
 import com.example.servicemanagement.models.ServiceProvider;
 import com.example.servicemanagement.services.SpService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
+@WebMvcTest
 public class SpControllerTest {
+    @Autowired
     private MockMvc mockMvc;
 
-    @InjectMocks
-    private SpController spController;
-
-    @Mock
+    @MockBean
     private SpService spService;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
     private ServiceProvider serviceProvider;
-    private final ObjectMapper objectMapper = new ObjectMapper();
 
     @BeforeEach
-    void setUp() {}
+    void setUp() {
+        String category = "DINE_IN";
+        serviceProvider = new ServiceProvider();
+        serviceProvider.setId("spid");
+        serviceProvider.setName("name");
+        serviceProvider.setEmail("email");
+        serviceProvider.setAddress("address");
+        serviceProvider.setPhoneNo("phoneNo");
+        serviceProvider.setCategory(Category.valueOf(category));
+    }
 
     @Test
-    public void addServiceProvider_test() throws Exception {
-        RequestSpDto requestSpDto = RequestSpDto.builder()
-                .email("email")
-                .name("name")
-                .phoneNo("phoneNo")
-                .address("address")
-                .category("category")
-                .build();
-        ResponseSpDto responseSpDto = ResponseSpDto.builder()
-                .spId("spId")
-                .email("emailId")
-                .name("name")
-                .phoneNo("phoneNo")
-                .address("address")
-                .category("category")
-                .build();
+    void getAllServiceProviders_WhenNoSP() throws Exception {
+        when(spService.getAllServiceProviders())
+                .thenReturn(new ArrayList<>());
 
-        Mockito.doReturn(responseSpDto).when(spService).createServiceProvider(Mockito.any());
-        mockMvc = MockMvcBuilders.standaloneSetup(spController).build();
-        mockMvc.perform(MockMvcRequestBuilders.post("/service-providers")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsBytes(requestSpDto)))
-                .andExpect(status().is4xxClientError());
+        mockMvc.perform(MockMvcRequestBuilders.get("/service-provider"))
+                .andExpect(status().is(200))
+                .andExpect(jsonPath("$").exists())
+                .andExpect(content().string("[]"));
+    }
+
+    @Test
+    void getAllServiceProviders_WhenSpExists() throws Exception {
+        List<ResponseSpDto> responseSpDtoList = new ArrayList<>();
+        responseSpDtoList.add(ResponseSpDto.from(serviceProvider));
+
+        when(spService.getAllServiceProviders())
+                .thenReturn(responseSpDtoList);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/service-provider"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(objectMapper.writeValueAsString(responseSpDtoList)));
     }
 }
