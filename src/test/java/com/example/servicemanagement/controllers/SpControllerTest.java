14d32a38554569e22ae8ac8a6182f8f67e45308e
Anubhab Nath <reachanubhav24@gmail.com>
2023-11-12 02:21:28 +0530

    Functional tests #2

--- src/test/java/com/example/servicemanagement/controllers/SpControllerTest.java
+++ src/test/java/com/example/servicemanagement/controllers/SpControllerTest.java
@@ -2,22 +2,27 @@ package com.example.servicemanagement.controllers;
 
 import com.example.servicemanagement.dtos.RequestSpDto;
 import com.example.servicemanagement.dtos.ResponseSpDto;
+import com.example.servicemanagement.exceptions.ValueNotAllowedException;
 import com.example.servicemanagement.models.Category;
 import com.example.servicemanagement.models.ServiceProvider;
 import com.example.servicemanagement.services.SpService;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -34,11 +39,20 @@ public class SpControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
-    private ServiceProvider serviceProvider;
+    private static ServiceProvider serviceProvider;
+    private static RequestSpDto requestSpDto;
 
-    @BeforeEach
-    void setUp() {
+    @BeforeAll
+    static void init() {
         String category = "DINE_IN";
+        requestSpDto = RequestSpDto.builder()
+                .email("email")
+                .name("name")
+                .phoneNo("phoneNo")
+                .address("address")
+                .category(category)
+                .build();
+
         serviceProvider = new ServiceProvider();
         serviceProvider.setId("spid");
         serviceProvider.setName("name");
@@ -46,6 +60,45 @@ public class SpControllerTest {
         serviceProvider.setAddress("address");
         serviceProvider.setPhoneNo("phoneNo");
         serviceProvider.setCategory(Category.valueOf(category));
+    }
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void addServiceProvider_ThrowsValueNotAllowedException() throws Exception {
+        when(spService.createServiceProvider(any(RequestSpDto.class)))
+                .thenThrow(ValueNotAllowedException.class);
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .post("/service-provider")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(requestSpDto)))
+                .andExpect(status().isNotAcceptable());
+    }
+
+    @Test
+    void addServiceProvider_ReturnsNewServiceProvider() throws Exception {
+        ResponseSpDto responseSpDto = ResponseSpDto.from(serviceProvider);
+        when(spService.createServiceProvider(any(RequestSpDto.class)))
+                .thenReturn(responseSpDto);
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .post("/service-provider")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(requestSpDto)))
+                .andExpect(status().isCreated());
+    }
+
+    @Test
+    void retrieveServiceProviderById_ReturnsSpOfGivenSpId() throws Exception {
+        ResponseSpDto responseSpDto = ResponseSpDto.from(serviceProvider);
+        when(spService.getServiceProviderById(anyString()))
+                .thenReturn(responseSpDto);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/service-provider/12"))
+                .andExpect(status().isOk());
     }
 
     @Test
